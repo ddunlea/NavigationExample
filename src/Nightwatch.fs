c983ebcbd4f04ce9c63b1c64e541d0320047b2ce
ddunlea <ddunlea@gmail.com>
2017-12-29 13:11:39 +0000

    NavigatorIOS compiles but crashes

--- src/Nightwatch.fs
+++ src/Nightwatch.fs
@@ -4,8 +4,10 @@ open Elmish
 open Elmish.React
 open Elmish.ReactNative
 open Elmish.HMR
+open Fable.Core.JsInterop
 open Fable.Helpers.ReactNative
 open Fable.Helpers.ReactNative.Props
+open Fable.Import.ReactNative
 
 type Msg =
 | Noop
@@ -22,14 +24,11 @@ let init() =
 let view (model:Model) (dispatch: Msg -> unit) =
   match model with
   | BlankModel -> 
-    text [
-      TextProperties.Style [
-        FontSize 20.
-        FontWeight FontWeight.Bold
-        Margin 20.
-      ]
-    ] "Hello World"
-
+    // The prop `initialRoute` is marked as required in `NavigatorIOS`
+    let route: Route = createEmpty<Route>
+    route.``component`` <- Some (createElement(Fable.Import.ReactNative.Globals.View, [], []))
+    let initialRoute = NavigatorIOSProperties.InitialRoute route
+    createElement(Fable.Import.ReactNative.Globals.NavigatorIOS, [initialRoute], [])
 
 Program.mkProgram init update view
 #if RELEASE
